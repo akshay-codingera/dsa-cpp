20930121c4a4aaaa0a4b67c1b5b636f9fd210fdb
akshay-codingera <akshaybutola2005@gmail.com>
2026-01-01 14:18:19 +0530

    Create day01_begining.cpp
    Initial C++ setup and GitHub workflow .
    For the proper development of the repository for DSA-C++.

---
+++ day01_begining.cpp
@@ -0,0 +1,9 @@
+/*       this repository is all about the data structure and algorithms pattern wise problem solving          */
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<< " hello welcome to the coding_era with Akshay ";
+    return 0;
+}
