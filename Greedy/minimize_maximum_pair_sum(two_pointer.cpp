1a7ba9a75ea20e3caa94318aa70c8b41a24c096e
akshay-codingera <akshaybutola2005@gmail.com>
2026-01-24 23:44:29 +0530

    minimize_maximize_pair_add.cpp
    medium leetcode problem

---
+++ Greedy/minimize_maximum_pair_sum(two_pointer.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+class Solution {
+public:
+    int minPairSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        
+        int i = 0, j = nums.size() - 1;
+        int maxSum = 0;
+
+        while (i < j) {
+            maxSum = max(maxSum, nums[i] + nums[j]);
+            i++;
+            j--;
+        }
+
+        return maxSum;
+    }
+};
