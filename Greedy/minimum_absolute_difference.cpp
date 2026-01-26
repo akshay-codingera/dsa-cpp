5b1427d97202773734d664f946e0747f16fd15e6
akshay-codingera <akshaybutola2005@gmail.com>
2026-01-26 23:31:46 +0530

    minimum absolute difference
    this is a leetcode question

---
+++ Greedy/minimum_absolute_difference.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+#include<algorithm>
+#include <climits>
+
+
+class Solution {
+public:
+    vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+
+        int minDiff = INT_MAX;
+        vector<vector<int>> result;
+
+        // Find minimum difference
+        for (int i = 1; i < arr.size(); i++) {
+            minDiff = min(minDiff, arr[i] - arr[i - 1]);
+        }
+
+        // Collect all pairs with that difference
+        for (int i = 1; i < arr.size(); i++) {
+            if (arr[i] - arr[i - 1] == minDiff) {
+                result.push_back({arr[i - 1], arr[i]});
+            }
+        }
+
+        return result;
+    }
+};
