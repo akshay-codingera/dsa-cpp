b6728674561e3d5b7f2a321d45af6dfb87b9eef4
akshay-codingera <akshaybutola2005@gmail.com>
2026-02-04 23:36:00 +0530

    second largest digit in the string
    easy leetcode

---
+++ String/second largest_digit_in_string.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    int secondHighest(string s) {
+        int largest = -1, second = -1;
+
+        for (char c : s) {
+            if (isdigit(c)) {
+                int d = c - '0';
+                if (d > largest) {
+                    second = largest;
+                    largest = d;
+                } else if (d < largest && d > second) {
+                    second = d;
+                }
+            }
+        }
+        return second;
+    }
+};
