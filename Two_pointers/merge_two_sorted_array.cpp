84a2f01658147690c30262c2a8014cf5a17bbaef
akshay-codingera <akshaybutola2005@gmail.com>
2026-02-05 23:28:00 +0530

    merge two sorted array
    hi this is a easy leetcode question

---
+++ Two_pointers/merge_two_sorted_array.cpp
@@ -0,0 +1,24 @@
+#include<vector>
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums, int n) {
+        int i = m-1; 
+        int j = n-1; 
+        int k = m+n-1; 
+
+        while ( i >= 0 && j>= 0)
+        {
+            if(nums1[i] >= nums[j])
+            { nums1[k] = nums1[i]; i--; k--;} 
+            else { nums1[k] = nums[j]; j--; k--;}
+        }
+        
+
+        while(j>=0)
+        {
+            nums1[k] = nums[j]; j--; k--;
+        }
+    }
+};
