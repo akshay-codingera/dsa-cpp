a0e1d72fe27e05148f24b3a553bb84f8970b05c2
akshay-codingera <akshaybutola2005@gmail.com>
2026-02-11 00:27:28 +0530

    power of 4
    recursion

---
+++ Recursion/power_ of_4.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if(n<0) {return false;}
+        if(n == 1){ return true;}
+if ( ((n & (n - 1)) == 0) && ((n - 1) % 3 == 0) )
+
+       { return true;}
+    else return false;
+    }
+};
