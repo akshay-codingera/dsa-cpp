b484cd8831a043692e193c002e14016c6aab706f
akshay-codingera <akshaybutola2005@gmail.com>
2026-02-10 23:47:13 +0530

    power of 3
    recursion problem

---
+++ Recursion/power_of_3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+// class Solution {
+// public:
+//     bool isPowerOfThree(int n) {
+//         if(n == -1 ) { return false;}
+//        if(n%3 == 0) {return true;}
+//        if((n%3 == 1 )||(n%3 == 2)) { return false;}
+      
+// };
+
+
+
+// we have another method the highest number in the 32 bit system  3^19 % n == 0 and then return true ;
