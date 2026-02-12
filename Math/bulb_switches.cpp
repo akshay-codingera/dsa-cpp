84a3b45300284e1127faed7714a315f0dffae269
akshay-codingera <akshaybutola2005@gmail.com>
2026-02-12 23:51:20 +0530

    bulb switcher
    medium level math problem

---
+++ Math/bulb_switches.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<algorithm>
+#include <cmath>
+using namespace std;
+
+class Solution {
+public:
+    int bulbSwitch(int n) {int count;
+       int x = sqrt(n);
+       return x;
+    }
+};
