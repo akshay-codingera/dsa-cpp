4c56cfc90f7f79874057cdbe3450d5b4bd077493
akshay-codingera <akshaybutola2005@gmail.com>
2026-02-13 00:03:46 +0530

    update the bulb switcher
    medium solving in different complexity

--- Math/bulb_switches.cpp
+++ Math/bulb_switches.cpp
@@ -10,3 +10,40 @@ public:
        return x;
     }
 };
+
+/*
+class Solution {
+public:
+    int bulbSwitch(int n) {int count=0;
+       for(int i = 1; i<=n;i++)
+       {
+         for(int j = 1; j<= i; j++)
+         {
+             if(i/j==j){count++;continue;}
+         }
+       }
+       return count;
+    }
+};
+*/
+
+/*
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        int count = 0;
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j * j <= i; j++) {
+                if(j * j == i) {
+                    count++;
+                    break;   // stop checking this i
+                }
+            }
+        }
+
+        return count;
+    }
+};
+
+*/
