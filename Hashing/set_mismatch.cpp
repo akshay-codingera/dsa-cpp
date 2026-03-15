6b1f011c5d164fd1cdf50ec032662b771b2babf2
akshay-codingera <akshaybutola2005@gmail.com>
2026-03-15 23:16:16 +0530

    set mismatch
    leetcode hash map question

---
+++ Hashing/set_mismatch.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+#include<unordered_map>
+
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        
+        unordered_map<int,int> mp;
+        int n = nums.size();
+        
+        for(int num : nums){
+            mp[num]++;
+        }
+        
+        int duplicate = -1;
+        int missing = -1;
+        
+        for(int i=1;i<=n;i++){
+            if(mp[i] == 2)
+                duplicate = i;
+            if(mp[i] == 0)
+                missing = i;
+        }
+        
+        return {duplicate, missing};
+    }
+};
