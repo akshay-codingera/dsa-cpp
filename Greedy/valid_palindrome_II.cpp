32298d3abaa479eac37fa87469ed029a469a775c
akshay-codingera <akshaybutola2005@gmail.com>
2026-03-30 23:46:09 +0530

    valid_palindrome_II.cpp
    greedy problem

---
+++ Greedy/valid_palindrome_II.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+#include<bits\stdc++.h>
+
+class Solution {
+public:
+    bool isPalindrome(string &s, int left, int right) {
+        while (left < right) {
+            if (s[left] != s[right]) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    bool validPalindrome(string s) {
+        int left = 0, right = s.length() - 1;
+
+        while (left < right) {
+            if (s[left] != s[right]) {
+                return isPalindrome(s, left + 1, right) || 
+                       isPalindrome(s, left, right - 1);
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
