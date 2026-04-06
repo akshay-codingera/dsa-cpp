fcdcbbb31b1ed3257234473bda868775b97a5553
akshay-codingera <akshaybutola2005@gmail.com>
2026-04-06 23:20:33 +0530

    next permutation using recursion
    medium level

---
+++ Recursion/permutation.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void solve(vector<int>& nums, vector<vector<int>>& ans, vector<int>& temp, vector<bool>& used) {
+
+        if (temp.size() == nums.size()) {
+            ans.push_back(temp);
+            return;
+        }
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (used[i]) continue;
+
+            temp.push_back(nums[i]);
+            used[i] = true;
+
+            solve(nums, ans, temp, used);
+
+         
+            temp.pop_back();
+            used[i] = false;
+        }
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> temp;
+        vector<bool> used(nums.size(), false);
+
+        solve(nums, ans, temp, used);
+        return ans;
+    }
+};
