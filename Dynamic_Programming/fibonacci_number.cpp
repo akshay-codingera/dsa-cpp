d50487a4d967c88d8016dac97d586300a000157f
akshay-codingera <akshaybutola2005@gmail.com>
2026-04-09 23:51:36 +0530

    fibonacci number
    dp programming leetcode number

---
+++ Dynamic_Programming/fibonacci_number.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+#include<bits/stdc++.h>
+class Solution {
+public:
+    int solve(int n, vector<int>& dp) {
+        if (n <= 1) return n;
+        if (dp[n] != -1) return dp[n];
+
+        return dp[n] = solve(n-1, dp) + solve(n-2, dp);
+    }
+
+    int fib(int n) {
+        vector<int> dp(n+1, -1);
+        return solve(n, dp);
+    }
+};
