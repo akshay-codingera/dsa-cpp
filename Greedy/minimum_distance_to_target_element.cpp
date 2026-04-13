dc88c5d846751192f4a5898ef86a31da91000500
akshay-codingera <akshaybutola2005@gmail.com>
2026-04-13 23:56:36 +0530

    minimum distance to trace a element
    greedy  , kind of array traversal

---
+++ Greedy/minimum_distance_to_target_element.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+#include<cmath>
+#include<algorithm>
+#include<bit/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int getMinDistance(vector<int>& nums, int target, int start) {
+        int minDist = INT_MAX;
+
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] == target) {
+                minDist = min(minDist, abs(i - start));
+            }
+        }
+
+        return minDist;
+    }
+};
