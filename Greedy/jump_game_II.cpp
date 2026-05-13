9d436547d0e0241847bcb879b82c17f57595e6ef
akshay-codingera <akshaybutola2005@gmail.com>
2026-05-13 20:32:11 +0530

    jump game II
    greedy leetcode solution

---
+++ Greedy/jump_game_II.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int jumps = 0;
+        int currentEnd = 0;
+        int farthest = 0;
+
+        for (int i = 0; i < static_cast<int>(nums.size()) - 1; i++) {
+            farthest = max(farthest, i + nums[i]);
+
+            if (i == currentEnd) {
+                jumps++;
+                currentEnd = farthest;
+            }
+        }
+
+        return jumps;
+    }
+};
+
+int main() {
+    Solution sol;
+
+    vector<int> nums = {2, 3, 1, 1, 4};
+
+    cout << "Minimum jumps: " << sol.jump(nums) << endl;
+
+    return 0;
+}
