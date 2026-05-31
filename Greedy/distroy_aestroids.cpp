73e3e530a20c0560f2ef84349d3a55d16049a30d
akshay-codingera <akshaybutola2005@gmail.com>
2026-05-31 22:15:33 +0530

    destroy asteroids
    greedy and sorting question

---
+++ Greedy/distroy_aestroids.cpp
@@ -0,0 +1,33 @@
+// problem related to sorting and greedy approach
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
+    sort(asteroids.begin(), asteroids.end());
+
+    long long currentMass = mass;
+
+    for (int asteroid : asteroids) {
+        if (currentMass < asteroid) {
+            return false;
+        }
+
+        currentMass += asteroid;
+    }
+
+    return true;
+}
+
+int main() {
+    int mass = 10;
+    vector<int> asteroids = {3, 9, 19, 5, 21};
+
+    if (asteroidsDestroyed(mass, asteroids))
+        cout << "true";
+    else
+        cout << "false";
+
+    return 0;
+}
