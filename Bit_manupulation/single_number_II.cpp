08f4be3e6ef4ee677268ed7894d945fce8f82ef9
akshay-codingera <akshaybutola2005@gmail.com>
2026-06-07 23:33:38 +0530

    single number II
    leetcode
    bits manipulation

---
+++ Bit_manupulation/single_number_II.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int result = 0;
+
+        for (int bit = 0; bit < 32; bit++) {
+            int count = 0;
+
+            for (int num : nums) {
+                if ((num >> bit) & 1) {
+                    count++;
+                }
+            }
+
+            if (count % 3 != 0) {
+                result |= (1 << bit);
+            }
+        }
+
+        return result;
+    }
+};
+
+int main() {
+    vector<int> nums = {2, 2, 3, 2};
+
+    Solution obj;
+    cout << obj.singleNumber(nums);
+
+    return 0;
+}
