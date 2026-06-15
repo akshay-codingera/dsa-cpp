9676d31422d7aea3e8e52fab13b3e77239ccca40
akshay-codingera <akshaybutola2005@gmail.com>
2026-06-15 23:24:38 +0530

    slime on lime
    codeforces

---
+++ Math/slimes_on_a_lime.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> v(n);
+
+        for (int i = 0; i < n; i++)
+            cin >> v[i];
+
+        int mn = *min_element(v.begin(), v.end());
+        int mx = *max_element(v.begin(), v.end());
+
+        cout << (mx - mn + 1) / 2 << '\n';
+    }
+
+    return 0;
+}
