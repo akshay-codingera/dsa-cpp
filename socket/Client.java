053a552279b1d0c51b6ad37fa0fed1ac5e46196c
akshay-codingera <akshaybutola2005@gmail.com>
2026-07-10 22:20:29 +0530

    update
    update

--- socket/Client.java
+++ socket/Client.java
@@ -3,6 +3,7 @@ import java.net.*;
 
 public class Client {
 public static void main(String[] args) {
+    // exception handling the try and catch handling block
 try {
 Socket socket = new Socket("127.0.0.1", 5000);
 
