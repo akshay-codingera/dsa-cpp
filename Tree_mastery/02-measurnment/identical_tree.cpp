f9a011b82ed227f81e592a3b8700982c323bfca2
Akshay Butola <akshaybutola2005@gmail.com>
2026-08-04 23:48:55 +0530

    identical tree or the same tree
    finding if the both trees entered are same or not

---
+++ Tree_mastery/02-measurnment/identical_tree.cpp
@@ -0,0 +1,32 @@
+// same tree or the identical tree 
+
+ // Definition for a binary tree node.
+ struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ }
+
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p== NULL && q == NULL) return true;
+        if(p == NULL || q == NULL) return false;
+
+       bool isleft = isSameTree(p->left,q->left);
+       bool isright = isSameTree(p->right,q->right);
+
+       return((isleft && isright) && (p->val == q->val));        
+    }
+};
+
+
+int main()
+{
+new class Solution();
+return 0;
+}
