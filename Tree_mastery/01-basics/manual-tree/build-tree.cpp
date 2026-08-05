6bf4d5c1ca61db808190e5d2e90963cf8f789381
Akshay Butola <akshaybutola2005@gmail.com>
2026-08-05 21:04:04 +0530

    started tree building
    checking for the mastery tree

--- Tree_mastery/01-basics/manual-tree/build-tree.cpp
+++ Tree_mastery/01-basics/manual-tree/build-tree.cpp
@@ -1,1 +1,35 @@
 // tree as a data structure is a hierachical data structure 
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    public: 
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void buildTree(Node* root)
+{
+    cout << "enter the data for the node: : ";
+    int data;
+    cin >> data;
+    root = new Node(data);
+    cout << "enter the data for the left child of " << data << endl;
+    buildTree(root->left);
+    cout << "enter the data for the right child of " << data<< endl;
+    buildTree(root->right);
+}
+
+int main()
+{
+    Node* root = NULL;
+    buildTree(root);
+    return 0;
+}
--- Tree_mastery/01-basics/node-defination/node.cpp
+++ Tree_mastery/01-basics/node-defination/node.cpp
@@ -1,12 +1,22 @@
 // node structure for different nodes or tree 
+#include<bits/stdc++.h>
+using namespace std;
 
-/*
 struct Node {
     int data; 
     Node* left;
     Node* right;
-    /// else on itself
+
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+
+int main()
+{
+    return 0;
 }
-
-
-*/
